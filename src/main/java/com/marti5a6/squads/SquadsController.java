e609c23bbc077834494636e2d44746d5d8b0d007
Tony <marti5a6@mail.uc.edu>
2022-10-24 15:19:34 -0400

    ui changes

--- src/main/java/com/marti5a6/squads/SquadsController.java
+++ src/main/java/com/marti5a6/squads/SquadsController.java
@@ -3,18 +3,11 @@ package com.marti5a6.squads;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
 public class SquadsController {
-	
 	@Autowired
-	
-	@RequestMapping(value="/", method=RequestMethod.GET)
-	public String read() {
-		return "index";
-	}
 	
 	@RequestMapping("/")
 	public String index() {
