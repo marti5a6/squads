c7512f3e84384e6aabc729b677b4a1c07276ac4d
Pranav Mahajan <pranavm7@outlook.com>
2022-11-23 20:56:05 -0500

    Added JavaDoc to Role.java

--- src/main/java/com/squads/entity/Role.java
+++ src/main/java/com/squads/entity/Role.java
@@ -5,6 +5,11 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/**
+* Role Entity is used for user role management.
+* Interacts with 'authorities' table in the database.
+* @author <<Insert author>>
+*/
 @Entity
 @Table(name="authorities")
 public class Role {
@@ -20,29 +25,57 @@ public class Role {
 		
 	}
 	
+/**
+* Constructor for the Role class
+* @param username - The username created by the user.
+* @param role - Roles assigned to the user. (Default: "ROLE_USER")
+* @return None
+*/
 	public Role(String username, String role) {
 		this.username = username;
 		this.role = "ROLE_USER";
 	}
 
 	// Getters and Setters
+/**
+* Returns the username for the current user object during runtime.
+* @return [String] : Username of the current user.
+*/
 	public String getUsername() {
 		return username;
 	}
 
+/**
+* Sets the username for the current user object during runtime.
+* @param username - Username to be assigned.
+*/
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
+/**
+* Returns the role for the current user object during runtime.
+* @return [String] : Username of the current user.
+*/
     public String getRole() {
 		return role;
 	}
+
+/**
+* Sets the role for a user object.
+* @param role - Role to be assigned to the user.
+*/
 
 	public void setRole(String role) {
 		this.role = "ROLE_USER";
 	}
 
 	// toString method
+
+/**
+* Returns the details of the user object.
+* @return [String] : Formatted as [username=$Username, role=$role]
+*/
 	@Override
 	public String toString() {
 		return "User [username=" + username + ", role=" + role + "]";
